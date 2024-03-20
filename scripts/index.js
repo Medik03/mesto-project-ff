4c3edcd10cbf1ff785983a20d7cbf8ade4fb7eb3
Denis Belchenko <denis.belchenko03@yandex.ru>
2024-03-20 13:28:10 +0300

    готово создание карточки и удаление

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const template = document.querySelector('#card-template').content;
+const galeryList = document.querySelector('.places__list');
+
+function createCard (cardElem, deleteCard) {
+    
+    const cardElement = template.querySelector('.card').cloneNode(true);
+    const delCardButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__title').textContent = cardElem.name;
+    cardElement.querySelector('.card__image').src = cardElem.link;
+    cardElement.querySelector('.card__image').alt = cardElem.name;
+    delCardButton.addEventListener('click', function() {
+        deleteCard(cardElement)
+    }) ; 
+    return cardElement;
+}
+function deleteCard(cardElement) {
+    cardElement.closest('.card').remove();
+}
+  
+function addCard(cardElement) {
+   galeryList.append(cardElement);
+  };
+
+ 
+initialCards.forEach((cardElem) => {
+    galeryList.append(createCard(cardElem, deleteCard))
+})
